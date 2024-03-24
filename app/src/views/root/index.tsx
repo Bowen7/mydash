f4e6081afb55b030e3e55766ce0c21e771fbdeb8
Bowen7 <zwxdyx@foxmail.com>
2024-03-24 20:57:46 +0800

    Use svh to fix the mobile screen

--- app/src/views/root/index.tsx
+++ app/src/views/root/index.tsx
@@ -3,7 +3,7 @@ import { Header } from './header';
 
 export const Root = () => {
   return (
-    <div className="w-screen h-screen flex flex-col">
+    <div className="w-screen h-svh flex flex-col overflow-hidden">
       <Header />
       <div className="flex-1 overflow-auto flex justify-center">
         <Outlet />
